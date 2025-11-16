ee38fb79ed7a1ccb30c17e7b6e7fc8a12e719c0c
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-11-16 13:21:26 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const express=require('express');
 const app=express();
+const cors=require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port=process.env.port || 3000;
+
+app.use(cors());
+app.use(express.json()); 
+
+
+//
+//
+
+
+const uri = "mongodb+srv://Travel-agency-project:ZNNYA5Eeo8OFPJmF@cluster0.6pjurty.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run(){
+    try{
+          await client.connect();
+          await client.db('admin').command({ping:1});
+          console.log("Connect with database");
+
+          const database=client.db('travelData');
+          const dataCollection=database.collection("properties")
+    }
+
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send("hellow Travel agency")
