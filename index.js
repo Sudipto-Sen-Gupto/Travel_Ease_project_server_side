3701385987be7bc184fb1e645cae218a8a9af957
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-12-09 21:50:27 +0600

    api for all vehicle added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const cors=require('cors')
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port=process.env.port || 3000;
 
@@ -12,7 +13,7 @@ app.use(express.json());
 //
 
 
-const uri = "mongodb+srv://Travel-agency-project:ZNNYA5Eeo8OFPJmF@cluster0.6pjurty.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6pjurty.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -29,8 +30,23 @@ async function run(){
           await client.db('admin').command({ping:1});
           console.log("Connect with database");
 
-          const database=client.db('travelData');
+          const database=client.db('TravelDB');
           const dataCollection=database.collection("properties")
+          
+          //home page
+          app.get('/properties',async(req,res)=>{
+            const cursor=dataCollection.find().limit(6);
+            const result=await cursor.toArray();
+            res.send(result);
+
+            //all vehicles page
+            app.get('/allProperty',async(req,res)=>{
+                 
+                const result=await dataCollection.find().toArray();
+                 res.send(result)
+                
+            })
+          })
     }
 
     finally{
